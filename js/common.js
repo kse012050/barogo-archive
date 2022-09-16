291dafba466f15b25ca7ee2f38395489c25f9feb
김성은 <kse012050@naver.com>
2022-09-16 13:21:31 +0900

    분류 click 만들었다

--- js/common.js
+++ js/common.js
@@ -1,42 +1,49 @@
 $(document).ready(function(){
-    let listBox = $('.listBox');
-    let listBoxWidth = $('.listBox').width();
-    let listCount = Math.ceil(listBoxWidth / 600);
-    listCount > 4 && (listCount = 4);
-    let list = $('.listBox').find('li');
-    let listGap = 20;
-    let listArray = []
-    let listWidth = (listBoxWidth - (listGap * (listCount - 1))) / listCount
-    for(let a = 0; a < listCount; a++){
-        listArray.push(0);
+    listLayout();
+    $(window).resize(function(){
+        listLayout();
+    })
+
+    function listLayout(){
+        let listBox = $('.listBox');
+        let listBoxWidth = $('.listBox').width();
+        let listCount = Math.ceil(listBoxWidth / 600);
+        listCount > 4 && (listCount = 4);
+        let list = $('.listBox').find('li');
+        let listGap = 20;
+        let listArray = []
+        let listWidth = (listBoxWidth - (listGap * (listCount - 1))) / listCount
+        for(let a = 0; a < listCount; a++){
+            listArray.push(0);
+        }
+    
+        list.css('width', listWidth);
+        list.each(function(i){
+            if($(this).css('display') == 'none'){return}
+            let numb = Math.floor(i % listCount);
+            numb = listArray.indexOf(Math.min(...listArray));
+            $(this).css({
+                left : (listWidth * numb) + (listGap * numb),
+                top : Math.min(...listArray)
+            })
+            listArray[numb] = $(this).height() + listGap + listArray[numb];
+        })
+        listBox.css('height',Math.max(...listArray));
+        
     }
 
-    list.css('width', listWidth);
-    list.each(function(i){
-        let numb = Math.floor(i % listCount);
-        let numb1 = 0;
-        // if(i < listCount){
-        //     $(this).css({
-        //         left : (listWidth * numb) + (listGap * numb),
-        //         top : 0
-        //     })
-        //     listArray[numb] = $(this).height() + listGap + listArray[numb];
-        // }else{
-        //     numb1 = listArray.indexOf(Math.min(...listArray));
-        //     $(this).css({
-        //         left : (listWidth * numb1) + (listGap * numb1),
-        //         top : Math.min(...listArray)
-        //     })
-        //     listArray[numb1] = $(this).height() + listGap + listArray[numb1];
-        // }
-
-        numb1 = listArray.indexOf(Math.min(...listArray));
-        $(this).css({
-            left : (listWidth * numb1) + (listGap * numb1),
-            top : Math.min(...listArray)
+    $('.btnBox li button').click(function(){
+        $('.btnBox li button').removeClass('active');
+        $(this).addClass('active');
+        let test = $(this).text();
+        let list = $('.listBox').find('li');
+        list.each(function(){
+            $(this).stop().fadeIn();
+            if($(this).find('mark').text().indexOf(test) < 0 && test != 'ALL'){
+                $(this).stop().hide();
+            }
         })
-        listArray[numb1] = $(this).height() + listGap + listArray[numb1];
+
+        listLayout();
     })
-    listBox.css('height',Math.max(...listArray));
-    
 })
